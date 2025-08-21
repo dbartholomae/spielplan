a6585aa034ad52851900a9423596b46b50438a09
Daniel Bartholomae <daniel@bartholomae.name>
2025-08-21 23:21:07 +0200

    Add favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,16 @@ import "./globals.css";
 export const metadata = {
   title: "Spielplan",
   description: "GameNight Scheduler",
+  themeColor: "#ffffff",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
